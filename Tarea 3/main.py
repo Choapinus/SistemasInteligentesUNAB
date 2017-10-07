dee3a8436244ebec74edeb3f30c81632f61dcae2
Don Choppy <wxtem@hotmail.com>
2017-10-07 16:30:11 -0300

    mas pruebas en la VM

--- Tarea 3/main.py
+++ Tarea 3/main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
 	cents = []
 
-	#generate_random_vectors('vectores.txt') #descomentar para quien no tenga sus vectores random creados
+	generate_random_vectors('vectores.txt') #descomentar para quien no tenga sus vectores random creados
 	cents_data = get_centroids('rand_vec.txt')
 
 	#cents_data = get_centroids_choppy('centroids_detail.txt')
@@ -12,9 +12,22 @@ if __name__ == '__main__':
 	for data in cents_data:
 		cents.append(Centroid(data))
 
+	print cents[0].name
+	print cents[0].data.__len__()
+	print cents[0].etiquetas
+	print
+	print cents[1].name
+	print cents[1].data.__len__()
+	print cents[1].etiquetas
+	print
+	print cents[2].name
+	print cents[2].data.__len__()
+	print cents[2].etiquetas
+	print
 	print cents[3].name
 	print cents[3].data.__len__()
 	print cents[3].etiquetas
+	print
 
 
 
