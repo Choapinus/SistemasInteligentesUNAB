39387f549cac1f362f98073085709c8ab67f386f
Don Choppy <wxtem@hotmail.com>
2017-10-07 17:18:03 -0300

    mas errores

--- Tarea 3/main.py
+++ Tarea 3/main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 		if(not (len(cents[0].data) > 1000 or len(cents[0].data) < 200 or 
 			len(cents[1].data) > 1000 or len(cents[1].data) < 200 or 
 			len(cents[2].data) > 1000 or len(cents[2].data) < 200 or 
-			len(cents[3].data) > 1000 or len(cents[3].data) < 200 or)):
+			len(cents[3].data) > 1000 or len(cents[3].data) < 200)):
 			print cents[0].name
 			print 'len data: ', cents[0].data.__len__()
 			print cents[0].etiquetas
@@ -37,6 +37,7 @@ if __name__ == '__main__':
 			print
 		else:
 			main()
+	main()
 
 
 
